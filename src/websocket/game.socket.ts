e723f57ab3b5ebd3c2682824642457c312124043
Patrick Rush <mail@patrickrush.tech>
2023-12-23 01:12:44 -0500

    Add basic db configuration

--- src/websocket/game.socket.ts
+++ src/websocket/game.socket.ts
@@ -1,9 +1,31 @@
 import { Socket } from "socket.io"
 import MySocketInterface from "./mySocketInterface"
 
-class GameSocket implements MySocketInterface {
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
-   handleConnection(socket: Socket) {
+
+
+class GameSocket implements MySocketInterface {
+    
+    async handleConnection(socket: Socket) {
+        const uri = `mongodb+srv://nerts_admin:${encodeURIComponent(process.env.MONGO_DB_PASSWORD)}@nerts.kyvvyac.mongodb.net/?retryWrites=true&w=majority`
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true
+            }
+        })
+        console.log("uri:", uri)
+        try {
+            await client.connect()
+            await client.db("nerts_db").command({ ping: 1 })
+            console.log("pinged db")
+        } catch (err) {
+            console.error("error connecting to db")
+        } finally {
+            await client.close()
+        }
         const gameState = []
         socket.emit('ping', 'Hi! I am a live socket connection')
         socket.on('game', (msg) => {
--- src/websocket/mySocketInterface.ts
+++ src/websocket/mySocketInterface.ts
@@ -3,7 +3,7 @@ import { Socket } from "socket.io"
 interface MySocketInterface {
 
    handleConnection(socket: Socket): void
-   middlewareImplementation?(soccket: Socket, next: any): void
+   middlewareImplementation?(socket: Socket, next: any): void
 
 }
 
